b0ff6e119c19150c5bd671000aa5535210ae1381
Adiputra Arya Sena <aryasena0902@gmail.com>
2026-08-27 12:15:04 +0700

    fix fallback css link for production

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jejak Purba — Ekspedisi Waktu</title>
     
-    <!-- Diperbaiki agar terbaca dengan benar di Railway -->
+    <!-- Vite bawaan + Cadangan CSS publik agar aman di Railway -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('build/assets/app-DheWjRpS.css') }}">
+
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     
     <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@700;900&family=Plus+Jakarta+Sans:wght@500;700;800&display=swap" rel="stylesheet">
